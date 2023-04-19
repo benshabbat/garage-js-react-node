a5956be0188aa8cf6facca7489fc0a3328b520e5
benshabbat <benshabbat27@gmail.com>
2023-04-19 15:40:05 +0300

    Add utils and changes index and app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./pages/login/Login.jsx";
+import Register from "./pages/Register.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header/Header.jsx";
+import { ToastContainer } from "react-toastify";
+import Account from "./pages/account/Account.jsx";
+import "react-toastify/dist/ReactToastify.css";
+import Services from "./pages/services/Services.jsx";
+import ReqService from "./pages/reqService/ReqService.jsx";
+import Messages from "./pages/messages/Messages.jsx";
+import PageLanding from "./pages/pageLanding/PageLanding.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+          <Header />
+        <Routes>
+            <Route path="/" element={<PageLanding />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/messages" element={<Messages />} />    
+            <Route path="/services/user/" element={<Services />} />
+            <Route path="/services/car/:carId" element={<Services />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+    </>
   );
 }
-
 export default App;
