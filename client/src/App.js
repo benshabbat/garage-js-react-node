c4a7141d10cec144913b1eea19c482980aabf159
benshabbat <benshabbat27@gmail.com>
2023-04-22 21:53:55 +0300

    create index for pages and and create register folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
-import Login from "./pages/login/Login.jsx";
 import Register from "./pages/Register.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Header from "./components/header/Header.jsx";
 import { ToastContainer } from "react-toastify";
 import Account from "./pages/account/Account.jsx";
 import "react-toastify/dist/ReactToastify.css";
 import Services from "./pages/services/Services.jsx";
-import ReqService from "./pages/reqService/ReqService.jsx";
 import Messages from "./pages/messages/Messages.jsx";
 import PageLanding from "./pages/pageLanding/PageLanding.jsx";
 
@@ -15,14 +12,14 @@ function App() {
   return (
     <>
       <BrowserRouter>
-          <Header />
+        <Header />
         <Routes>
-            <Route path="/" element={<PageLanding />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/account" element={<Account />} />
-            <Route path="/messages" element={<Messages />} />    
-            <Route path="/services/user/" element={<Services />} />
-            <Route path="/services/car/:carId" element={<Services />} />
+          <Route path="/" element={<PageLanding />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/services/user/" element={<Services />} />
+          <Route path="/services/car/:carId" element={<Services />} />
         </Routes>
       </BrowserRouter>
       <ToastContainer />
---
+++ client/src/pages/index.js
@@ -0,0 +1,10 @@
+export { default as Account } from "./account/Account";
+export { default as Home } from "./home/Home";
+export { default as Login } from "./login/Login";
+export { default as Messages } from "./messages/Messages";
+export { default as PageLanding } from "./pageLanding/PageLanding";
+export { default as Services } from "./services/Services";
+export { default as ReqService } from "./reqService/ReqService";
+export { default as Register } from "./register/Register";
+
+
