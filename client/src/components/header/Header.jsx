5cb9c5ad8360b9cecf004a96ec621a7e8dd1cd8b
benshabbat <benshabbat27@gmail.com>
2023-04-22 22:02:13 +0300

    changes on header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,19 +2,33 @@ import "./header.css";
 import { Link, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { NavUser, NavLanding } from "../index";
-
+import MyAccount from "../myAccount/MyAccount";
 const Header = () => {
   const { user } = useSelector((state) => state.auth);
   // const { _id } = useSelector((state) => state.user.user);
 
+  //   return (
+  //     <>
+  //       <nav className="header-container">
+  //         <div className="logo">
+  //           <Link to="/">Garage770</Link>
+  //         </div>
+  //         <div>{user ? <NavUser /> : <NavLanding />}</div>
+  //       </nav>
+  //       <Outlet />
+  //     </>
+  //   );
+  // };
+
+  // export default Header;
   return (
     <>
-      <header className="header-container">
+      <div className="navbar">
         <div className="logo">
           <Link to="/">Garage770</Link>
         </div>
-        <ul>{user ? <NavUser /> : <NavLanding />}</ul>
-      </header>
+        <div>{user ? <NavUser /> : <NavLanding />}</div>
+      </div>
       <Outlet />
     </>
   );
