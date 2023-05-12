28baa8eb364077ad7c2d4428a9a4513dde964d74
benshabbat <benshabbat27@gmail.com>
2023-05-12 14:10:23 +0300

    utils update

--- client/src/Utils.js
+++ client/src/Utils.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-
+const API_URL_CAR = "/cars";
+const API_URL_USER = "/users";
+const API_URL_SERVICE = "/services";
+const API_URL_MESSAGES = "/messages"
+const API_URL_REGISTER = "/auth/register";
+const ADMIN = "63e14deca4340e45d23f20b2"
 const getAll = (url) => {
   axios.get(url);
 };
@@ -9,16 +14,41 @@ const getById = (url, id) => {
 const addItem = (url, obj) => {
   axios.post(url, obj);
 };
-const updateItem = (url, id, obj) => {
-  axios.put(`${url}/${id}`, obj);
+const createUser = (obj) => {
+  axios.post(API_URL_REGISTER, obj);
 };
-const deleteItem = (url, id) => {
-  axios.delete(`${url}/${id}`);
+const updateUser = (id, obj) => {
+  axios.put(`${API_URL_USER}/${id}`, obj);
 };
-const getUserFullData = (id) => {
-  
+const updateService = (id, obj) => {
+  axios.put(`${API_URL_SERVICE}/${id}`, obj);
+};
+const updateCar = (id, obj) => {
+  axios.put(`${API_URL_CAR}/${id}`, obj);
+};
+const createCar = (id, obj) => {
+  axios.post(`${API_URL_CAR}/${id}`, obj);
+};
+const createService = (id, obj) => {
+  axios.post(`${API_URL_SERVICE}/${id}`, obj);
+};
+const createReqService = (obj) => {
+  axios.post(`${API_URL_MESSAGES}/to/${ADMIN}`, obj);
+};
+const createMessage = (obj) => {
+  axios.post(`${API_URL_MESSAGES}/to/${ADMIN}`, obj);
+};
+const deleteUser= (id) => {
+  axios.delete(`${API_URL_USER}/${id}`);
+};
+const deleteService= (id) => {
+  axios.delete(`${API_URL_SERVICE}/${id}`);
+};
+const deleteCar= (idCar ,idUser) => {
+  axios.delete(`${API_URL_CAR}/${idCar}/${idUser}`);
+};
+const getUser = (id) => {
+  return axios.get(`${API_URL_USER}/${id}`);
 };
 
-
-
-export { getAll, getById, addItem, updateItem, deleteItem };
+export {createMessage,createService,createUser,createReqService,updateService, getAll, getById, addItem, updateUser, deleteUser, createCar, getUser,deleteCar,updateCar,deleteService };
