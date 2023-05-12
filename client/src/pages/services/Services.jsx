b87e0cdf0c1f47b23ec2060da0ff00d7f1d4e05a
benshabbat <benshabbat27@gmail.com>
2023-05-12 14:01:40 +0300

    fix services

--- client/src/pages/services/Services.jsx
+++ client/src/pages/services/Services.jsx
@@ -1,40 +1,25 @@
-import "./services.css"
-import React, { useEffect } from "react";
+import "./services.css";
+import React from "react";
 import { useParams } from "react-router-dom";
 import Service from "../../components/service/Service";
-import { getCarsByIdUser } from "../../features/user/userSlice";
-import { useSelector, useDispatch } from "react-redux";
-const Services = () => {
-  const { _id } = useSelector((state) => state.auth.user);
-  const { user } = useSelector((state) => state.user);
+
+const Services = ({ user }) => {
   const { carId } = useParams();
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getCarsByIdUser(_id));
-  }, []);
-
   const carFilter = user?.cars?.filter((car) => car._id === carId);
-
+  const carService = (car) => {
+    return (
+      <div key={car?._id}>
+        <h2 className="title">{car?.numberPlate}</h2>
+        <Service carServices={car?.services} />
+      </div>
+    );
+  };
   return (
     <>
-      <h1 className="h-title">{`hello ${user.username}`}</h1>
-      {carId
-        ? carFilter.map((car) => {
-            return (
-              <div  key={car?._id}>
-                <h2 className="title">{car?.numberPlate}</h2>
-                <Service carServices={car?.services} />
-              </div>
-            );
-          })
-        : user?.cars?.map((car) => {
-            return (
-              <div key={car?._id}>
-                <h2 className="title">{car?.numberPlate}</h2>
-                <Service carServices={car?.services} />
-              </div>
-            );
-          })}
+      <h1 className="h-title">{`hello ${user?.username}`}</h1>
+      {carId && carFilter
+        ? carFilter?.map(carService)
+        : user?.cars?.map(carService)}
     </>
   );
 };
