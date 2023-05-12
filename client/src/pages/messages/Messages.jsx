6caffa76a74d6f4cbe388763435d11cec326fc88
benshabbat <benshabbat27@gmail.com>
2023-05-12 14:00:45 +0300

    fix messages

--- client/src/pages/messages/Messages.jsx
+++ client/src/pages/messages/Messages.jsx
@@ -1,53 +1,56 @@
-import "./messages.css";
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getMessagesByIdUser } from "../../features/user/userSlice";
-const Messages = () => {
-  const { messages } = useSelector((state) => state.user);
-  const { _id } = useSelector((state) => state.auth.user);
-
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getMessagesByIdUser(_id));
-  }, []);
+import "../../components/table/table.css";
+import CreateMessage from "../../components/create/CreateMessage";
+import useOpenModel from "../../hooks/useOpenModel";
+const Messages = ({ messages = null,user }) => {
+  const [handleCreateMessage, isOpenCreateMessage] = useOpenModel();
   return (
-    <div className="table">
-      <section className="table__header">
-        <h1>Messages</h1>
-        <div className="input-group">
-          <input type="search" placeholder="Search Data..." />
-        </div>
-      </section>
-      <section className="table__body">
-        <table className="table-messages">
-          <thead>
-            <tr>
-              <th>from</th>
-              <th>to</th>
-              <th>title</th>
-              <th>description</th>
-              <th>date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {messages &&
-              messages.map((message) => {
-                const dateArray = message.updatedAt.slice(0, 10).split("-");
-                const [year, month, day] = dateArray;
-                return (
-                  <tr key={message._id}>
-                    <td>{message.from?.username}</td>
-                    <td>{message.to?.username}</td>
-                    <td>{message.title}</td>
-                    <td>{message.description}</td>
-                    <td>{`${day}/${month}/${year}`}</td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
-      </section>
-    </div>
+    <>
+      <div className="table-container">
+        <section className="table__header">
+          <h1>Messages</h1>
+          <div className="input-group">
+            <input type="search" placeholder="Search Data..." />
+          </div>
+        </section>
+        <section className="table__body">
+          <table>
+            <thead>
+              <tr>
+                <th>from</th>
+                <th>to</th>
+                <th>title</th>
+                <th>description</th>
+                <th>date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {messages &&
+                messages.map((message) => {
+                  const dateArray = message?.updatedAt.slice(0, 10).split("-");
+                  const [year, month, day] = dateArray;
+                  return (
+                    <tr key={message?._id}>
+                      <td>{message?.from?.username}</td>
+                      <td>{message?.to?.username}</td>
+                      <td>{message?.title}</td>
+                      <td>{message?.description}</td>
+                      <td>{`${day}/${month}/${year}`}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </section>
+        <button onClick={handleCreateMessage}>New Message</button>
+      </div>
+      {
+        <CreateMessage
+          handelClick={handleCreateMessage}
+          isOpen={isOpenCreateMessage}
+          user={user}
+        />
+      }
+    </>
   );
 };
 
