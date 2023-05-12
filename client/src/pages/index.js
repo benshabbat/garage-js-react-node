6f823ed000df1e6697f51ee4f67c6552dc59732e
benshabbat <benshabbat27@gmail.com>
2023-05-12 14:02:09 +0300

    updated index

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,10 +1,10 @@
 export { default as Account } from "./account/Account";
 export { default as Home } from "./home/Home";
-export { default as Login } from "./login/Login";
 export { default as Messages } from "./messages/Messages";
 export { default as PageLanding } from "./pageLanding/PageLanding";
 export { default as Services } from "./services/Services";
-export { default as ReqService } from "./reqService/ReqService";
-export { default as Register } from "./register/Register";
+export { default as Users } from "./Users";
+export { default as Cars } from "./Cars";
+export { default as ServicesAdmin } from "./ServicesAdmin";
 
 
